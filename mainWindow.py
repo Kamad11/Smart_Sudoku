ba343b0fc00da0c318b1907619c074317ba21e30
ikathuria <ishani@kathuria.net>
2021-05-13 13:53:12 +0530

    upload mode complete

---
+++ mainWindow.py
@@ -0,0 +1,176 @@
+import os
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from utils import intialize_predection_model
+from upload import Ui_UploadImage
+from realTime import Ui_RealTime
+from randomGenerator import Ui_RandomGenerator
+
+style_sheet = """QPushButton { background-color: #F76B8A;
+                               color: #000000;
+                               border-radius: 10px; }
+QPushButton::hover { background-color: #ffffff;
+                     color: #F76B8A;
+                     border: 1px solid black; }
+QPushButton::pressed { background-color: #000000;
+                       color: #ffffff; }
+QPushButton::disabled { background-color: rgba(255, 255, 255, 0.5);
+                        color: #000000; }"""
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(850, 880)
+        MainWindow.setMinimumSize(QtCore.QSize(850, 880))
+        MainWindow.setStyleSheet("background-color: #ffffff;")
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setMinimumSize(QtCore.QSize(850, 850))
+        self.centralwidget.setObjectName("centralwidget")
+        self.main_layout = QtWidgets.QGridLayout(self.centralwidget)
+        self.main_layout.setObjectName("main_layout")
+
+        self.game = QtWidgets.QFrame(self.centralwidget)
+        self.game.setMinimumSize(QtCore.QSize(700, 700))
+        self.game.setObjectName("game")
+        self.game_layout = QtWidgets.QGridLayout(self.game)
+        self.game_layout.setObjectName("game_layout")
+
+        self.label = QtWidgets.QLabel(self.game)
+        font = QtGui.QFont()
+        font.setFamily("Goudy Stout")
+        font.setPointSize(60)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: #4592AF;")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.game_layout.addWidget(self.label, 0, 0, 1, 1)
+        self.main_layout.addWidget(self.game, 0, 0, 1, 1)
+        self.options = QtWidgets.QWidget(self.centralwidget)
+        self.options.setMinimumSize(QtCore.QSize(700, 90))
+        self.options.setStyleSheet(style_sheet)
+        self.options.setObjectName("options")
+        self.options_layout = QtWidgets.QGridLayout(self.options)
+        self.options_layout.setObjectName("options_layout")
+
+        self.real_time_button = QtWidgets.QPushButton(self.options)
+        self.real_time_button.setMinimumSize(QtCore.QSize(0, 80))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.real_time_button.setFont(font)
+        self.real_time_button.setObjectName("real_time_button")
+        self.options_layout.addWidget(self.real_time_button, 0, 0, 1, 1)
+
+        self.upload_button = QtWidgets.QPushButton(self.options)
+        self.upload_button.setMinimumSize(QtCore.QSize(0, 80))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.upload_button.setFont(font)
+        self.upload_button.setStyleSheet("")
+        self.upload_button.setObjectName("upload_button")
+        self.options_layout.addWidget(self.upload_button, 0, 1, 1, 1)
+
+        self.random_generator_button = QtWidgets.QPushButton(self.options)
+        self.random_generator_button.setMinimumSize(QtCore.QSize(0, 80))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.random_generator_button.setFont(font)
+        self.random_generator_button.setStyleSheet("")
+        self.random_generator_button.setObjectName("random_generator_button")
+        self.options_layout.addWidget(self.random_generator_button, 0, 2, 1, 1)
+        self.main_layout.addWidget(self.options, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 850, 30))
+        self.menubar.setMinimumSize(QtCore.QSize(0, 30))
+        self.menubar.setStyleSheet("color: #000000;")
+        self.menubar.setObjectName("menubar")
+        self.instructions = QtWidgets.QMenu(self.menubar)
+        self.instructions.setStyleSheet("")
+        self.instructions.setObjectName("instructions")
+        self.about = QtWidgets.QMenu(self.menubar)
+        self.about.setObjectName("about")
+        MainWindow.setMenuBar(self.menubar)
+        self.menubar.addAction(self.instructions.menuAction())
+        self.menubar.addAction(self.about.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "SMART SUDOKU"))
+        # buttons
+        # ## real time
+        self.real_time_button.setText(_translate("MainWindow", "Real time sudoku solver"))
+        self.real_time_button.clicked.connect(lambda: self.start_game('realTime'))
+
+        # ## upload
+        self.upload_button.setText(_translate("MainWindow", "Upload a sudoku image"))
+        self.upload_button.clicked.connect(lambda: self.start_game('upload'))
+
+        # ## random generator
+        self.random_generator_button.setText(_translate("MainWindow", "Generate a random game"))
+        self.random_generator_button.clicked.connect(lambda: self.start_game('randomGenerator'))
+
+        # menubar
+        self.instructions.setTitle(_translate("MainWindow", "Instructions"))
+        self.about.setTitle(_translate("MainWindow", "About"))
+
+    def start_game(self, game):
+        """Start game."""
+
+        self.replace = QtWidgets.QWidget()
+        self.replace.setMinimumSize(QtCore.QSize(700, 700))
+        # adding to games frame
+        self.game_layout.addWidget(self.replace, 0, 0, 1, 1)
+
+        the_game = False
+
+        if game == 'realTime':
+            realTime = QtWidgets.QWidget()
+            ui = Ui_RealTime()
+            ui.setupUi(realTime)
+
+            the_game = realTime
+
+        elif game == 'upload':
+            upload = QtWidgets.QWidget()
+            ui = Ui_UploadImage()
+
+            home_dir = str(os.getcwd())
+            img = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', home_dir)
+
+            model = intialize_predection_model()
+
+            ui.setupUi(upload, img[0], model)
+
+            the_game = upload
+
+        elif game == 'randomGenerator':
+            randomGenerator = QtWidgets.QWidget()
+            ui = Ui_RandomGenerator()
+            ui.setupUi(randomGenerator)
+
+            the_game = randomGenerator
+
+        if the_game:
+            self.game_layout.replaceWidget(self.replace, the_game)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+
+    MainWindow = QtWidgets.QMainWindow()
+
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ upload.py
@@ -0,0 +1,140 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import cv2
+import numpy as np
+import utils
+import sudukoSolver
+
+style_sheet = """QTabBar::tab { background-color: #ffffff;
+                                color: #000000;
+                                padding: 7px;
+                                border: 1px solid black; }
+QTabBar::tab::hover { background-color: rgba(255, 255, 255, 0.8); }
+QTabBar::tab::selected { background-color: #000000;
+                         color: #ffffff; }"""
+
+
+class Ui_UploadImage(object):
+    def setupUi(self, UploadImage, img, model):
+        # uploaded image
+        self.img_path = img
+        # digit recognition model
+        self.model = model
+
+        UploadImage.setObjectName("UploadImage")
+        UploadImage.resize(700, 700)
+        UploadImage.setMinimumSize(QtCore.QSize(700, 700))
+        UploadImage.setStyleSheet("background-color: #ffffff;")
+        self.main_layout = QtWidgets.QGridLayout(UploadImage)
+        self.main_layout.setObjectName("main_layout")
+
+        self.tab_widget = QtWidgets.QTabWidget(UploadImage)
+        self.tab_widget.setMinimumSize(QtCore.QSize(600, 600))
+        font = QtGui.QFont()
+        font.setFamily("Lucida Sans Typewriter")
+        font.setPointSize(10)
+        self.tab_widget.setFont(font)
+        self.tab_widget.setStyleSheet(style_sheet)
+        self.tab_widget.setUsesScrollButtons(False)
+        self.tab_widget.setObjectName("tab_widget")
+
+        self.unsolved_tab = QtWidgets.QWidget()
+        self.unsolved_tab.setStyleSheet("")
+        self.unsolved_tab.setObjectName("unsolved_tab")
+        self.unsolved_layout = QtWidgets.QGridLayout(self.unsolved_tab)
+        self.unsolved_layout.setObjectName("unsolved_layout")
+        self.unsolved_image = QtWidgets.QLabel(self.unsolved_tab)
+        self.unsolved_image.setMinimumSize(QtCore.QSize(600, 600))
+        self.unsolved_image.setPixmap(QtGui.QPixmap(self.img_path))
+        self.unsolved_image.setScaledContents(True)
+        self.unsolved_image.setObjectName("unsolved_image")
+        self.unsolved_layout.addWidget(self.unsolved_image, 0, 0, 1, 1)
+        self.tab_widget.addTab(self.unsolved_tab, "")
+
+        self.solved_tab = QtWidgets.QWidget()
+        self.solved_tab.setObjectName("solved_tab")
+        self.solved_layout = QtWidgets.QGridLayout(self.solved_tab)
+        self.solved_layout.setObjectName("solved_layout")
+        self.solved_image = QtWidgets.QLabel(self.solved_tab)
+        self.solved_image.setMinimumSize(QtCore.QSize(600, 600))
+        self.solved_image.setScaledContents(True)
+        self.solved_image.setObjectName("solved_image")
+        self.solved_layout.addWidget(self.solved_image, 0, 0, 1, 1)
+        self.tab_widget.addTab(self.solved_tab, "")
+        self.main_layout.addWidget(self.tab_widget, 0, 0, 1, 1)
+
+        self.retranslateUi(UploadImage)
+        self.tab_widget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(UploadImage)
+
+    def retranslateUi(self, UploadImage):
+        _translate = QtCore.QCoreApplication.translate
+
+        self.tab_widget.setTabText(self.tab_widget.indexOf(self.unsolved_tab), _translate("UploadImage", "Unsolved"))
+        self.tab_widget.setTabText(self.tab_widget.indexOf(self.solved_tab), _translate("UploadImage", "Solution"))
+        self.solved_image.setText(_translate("UploadImage", "Solving Sudoku..."))
+        self.sudoku_main()
+    
+    def sudoku_main(self):
+        heightImg = 450
+        widthImg = 450
+
+        # #### 1. prepare the image ################################################################
+        self.img = cv2.imread(self.img_path)
+        self.img = cv2.resize(self.img, (widthImg, heightImg))
+
+        self.imgThreshold = utils.pre_process(self.img)
+
+        # #### 2. find biggest out of all contours #################################################
+        contours, _ = cv2.findContours(
+            self.imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        biggest, _ = utils.biggest_contour(contours)
+
+        if biggest.size != 0:
+            biggest = utils.reorder(biggest)
+
+            pts1 = np.float32(biggest)
+            pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [
+                            widthImg, heightImg]])
+
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)  # GER
+            self.imgWarpColored = cv2.warpPerspective(self.img, matrix, (widthImg, heightImg))
+            self.imgWarpColored = cv2.cvtColor(self.imgWarpColored, cv2.COLOR_BGR2GRAY)
+
+            # #### 3. split the image and find digits ##############################################
+            self.imgSolvedDigits = np.zeros((heightImg, widthImg, 3), np.uint8)
+            boxes = utils.split_boxes(self.imgWarpColored)
+
+            numbers = utils.get_prediction(boxes, self.model)
+            numbers = np.asarray(numbers)
+
+            # 1 for empty cells, 0 for filled cells
+            posArray = np.where(numbers > 0, 0, 1)
+
+            # #### 4. find solution ################################################################
+            # converting to our board format
+            board = np.array_split(numbers, 9)
+
+            # try to find solution if one exists
+            try:
+                sudukoSolver.solve(board)
+            except:
+                pass
+
+            flatList = []
+            for sublist in board:
+                for item in sublist:
+                    flatList.append(item)
+            solvedNumbers = flatList*posArray
+            self.imgSolvedDigits = utils.display_numbers(self.imgSolvedDigits, solvedNumbers)
+
+            # overlay solution
+            # warp
+            pts2 = np.float32(biggest)
+            pts1 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            self.imgInvWarpColored = self.img.copy()
+            self.imgInvWarpColored = cv2.warpPerspective(self.imgSolvedDigits, matrix, (widthImg, heightImg))
+            self.inv_perspective = cv2.addWeighted(self.imgInvWarpColored, 1, self.img, 0.5, 1)
+            cv2.imwrite("Resources\\solved.jpg", self.inv_perspective)
+
+            self.solved_image.setPixmap(QtGui.QPixmap("Resources\\solved.jpg"))
